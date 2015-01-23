08ed6e6ca00f68a14ca83da98fa262eadcb1a24c
unknown <MRiley@C00146-01-13-15.csis.org>
2015-01-23 14:21:46 -0500

    I've completed the formulas to both makeCacheMatrix and cacheSolve. Both have been tested and both work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## establish s for later use
+    s<-NULL
+    
+    ## create the set function
+    set <<- function(y) {
+        x<<-y
+        S<<-NULL   
+    }
+    
+    ## Create a get, setsolve, and getsolve funtion inwhich you cache the
+    ## the inverse matrix
+    get <- function() x
+    setsolve <- function(solve) s<<-solve
+    getsolve <- function() s
+    
+    ## Output of all four fromulas
+    list(set = set, 
+         get = get, 
+         setsolve = setsolve, 
+         getsolve = getsolve)
+    
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Check to see if the inverse matrix is already chached.
+    ## If so, display that matrix
+    a <- x$getsolve()
+    if(!is.null(a)) {
+        message("getting chaged data")
+        return(a)
+    }
+    
+    ## If it isn't already cached, created the inverse matrix
+    data <- x$get()
+    a <- solve(data,...)
+    x$setsolve(a)
+    
+    ## Display the inverse matrix
+    a
+    
 }
